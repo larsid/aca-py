e49b76d3bd2908520a9e0493df7e94b8fe3a0e70
JoaoErick <jerick1415@outlook.com>
2022-09-27 13:00:16 -0300

    Feat: add CLI to the project

--- src/main/java/br/uefs/larsid/ariesagentclient/main/Main.java
+++ src/main/java/br/uefs/larsid/ariesagentclient/main/Main.java
@@ -8,12 +8,18 @@ import br.uefs.larsid.ariesagentclient.controller.Controller;
 import br.uefs.larsid.ariesagentclient.model.Credential;
 import br.uefs.larsid.ariesagentclient.model.CredentialDefinition;
 import br.uefs.larsid.ariesagentclient.model.Schema;
+import br.uefs.larsid.ariesagentclient.mqtt.Listener;
+import br.uefs.larsid.ariesagentclient.mqtt.MQTTClient;
+import br.uefs.larsid.ariesagentclient.util.CLI;
+
 import com.google.zxing.WriterException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import java.util.Scanner;
 import org.hyperledger.aries.api.connection.ConnectionRecord;
 import org.hyperledger.aries.api.connection.CreateInvitationResponse;
@@ -25,13 +31,58 @@ import org.hyperledger.aries.api.schema.SchemaSendResponse;
  */
 public class Main {
 
-    public static Controller controller;
-    public static void main(String[] args) throws IOException, WriterException {
-        final String AGENT_ADDR = "localhost";
-        final String AGENT_PORT = "8021";
-        final String AGENT_END_POINT = "https://2f8e-177-99-172-106.sa.ngrok.io";
+    public static boolean DEBUG_MODE = true;
 
-        controller = new Controller(AGENT_ADDR, AGENT_PORT, AGENT_END_POINT);
+    /* -------------------------- Topic constants ---------------------------- */
+    private static final String CREATE_INVITATION = "POST CREATE_INVITATION";
+    private static final String ACCEPT_INVITATION = "POST ACCEPT_INVITATION";
+    private static final String CREDENTIAL_DEFINITIONS = "POST CREDENTIAL_DEFINITIONS";
+    private static final String ISSUE_CREDENTIAL = "POST ISSUE_CREDENTIAL";
+    /* ---------------------------------------------------------------------- */
+
+    /* -------------------------- MQTT constants ---------------------------- */
+    private static final int MQTT_QOS = 1;
+    /* ---------------------------------------------------------------------- */
+
+    /* ---------------------- Controller properties ------------------------- */
+    private static String endpoint;
+    private static String credentialDefinitionId;
+    private static String brokerIp;
+    private static String brokerPort;
+    private static String brokerUsername;
+    private static String brokerPassword;
+    private static String agentIp;
+    private static String agentPort;
+    /* ---------------------------------------------------------------------- */
+
+    public static Controller controller;
+    private static MQTTClient mqttClient;
+
+    public static void main(String[] args) throws IOException, WriterException {
+        readProperties(args);
+
+        mqttClient = new MQTTClient(DEBUG_MODE, brokerIp, brokerPort, brokerUsername, brokerPassword);
+
+        mqttClient.connect();
+
+        String[] topics = {
+                CREATE_INVITATION,
+                ACCEPT_INVITATION,
+                CREDENTIAL_DEFINITIONS,
+                ISSUE_CREDENTIAL
+        };
+
+        new Listener(
+                mqttClient,
+                topics,
+                MQTT_QOS,
+                DEBUG_MODE);
+
+        // final String AGENT_ADDR = "localhost";
+        // final String AGENT_PORT = "8021";
+        // final String AGENT_END_POINT = "https://2f8e-177-99-172-106.sa.ngrok.io";
+
+        controller = new Controller(agentIp, agentPort, endpoint);
 
         /*Base to create issuer class*/
         
@@ -39,7 +90,6 @@ public class Main {
         
         /*Implementar demais métodos, verificação, recepção, ...*/
         
-        int idConvite = controller.getConnections().size();
         int idSchema = controller.getSchemasCreated().size()+10; //precisa automatizar o número baseado na persistencia
         int idTag = 1;
 
@@ -63,71 +113,115 @@ public class Main {
         values.put("matricula", "12345");
         credential.addValues(values);
 
-        final Scanner scan = new Scanner(System.in);
+        // final Scanner scan = new Scanner(System.in);
 
-        boolean menuControl = true;
+        // boolean menuControl = true;
 
-        do {
-            System.out.println("Menu Aries Agent\n");
-            System.out.println("1  - Gerar url de Conexão");
-            System.out.println("2  - Criar Definição de credencial");
-            System.out.println("3  - Emitir Credencial");
-            System.out.println("4  - Exibir Schemas Criados");
-            System.out.println("5  - Exibir schema por ID");
-            System.out.println("6  - Listar Conexões");
-            System.out.println("7  - Listar Definições de credenciais");
-            System.out.println("8  - Solicitar prova de credenciais");
-            System.out.println("9  - Verificar apresentação de prova de credenciais");
-            System.out.println("10 - Aceitar Conexão");
-            System.out.println("11 - Listar Credenciais Recebidas");
-            System.out.println("12 - Revogar Credencial Emitida");
-            System.out.println("0  - Exit\n");
+        // do {
+        //     System.out.println("Menu Aries Agent\n");
+        //     System.out.println("1  - Gerar url de Conexão");
+        //     System.out.println("2  - Criar Definição de credencial");
+        //     System.out.println("3  - Emitir Credencial");
+        //     System.out.println("4  - Exibir Schemas Criados");
+        //     System.out.println("5  - Exibir schema por ID");
+        //     System.out.println("6  - Listar Conexões");
+        //     System.out.println("7  - Listar Definições de credenciais");
+        //     System.out.println("8  - Solicitar prova de credenciais");
+        //     System.out.println("9  - Verificar apresentação de prova de credenciais");
+        //     System.out.println("10 - Aceitar Conexão");
+        //     System.out.println("11 - Listar Credenciais Recebidas");
+        //     System.out.println("12 - Revogar Credencial Emitida");
+        //     System.out.println("0  - Exit\n");
 
-            switch (scan.nextInt()) {
-                case 1: //Cria um convite de conexão
-                    createInvitation(controller, ("Convite_" + idConvite++));
-                    break;
-                case 2: //Cria uma definição de credencial
-                    createCredentialDefinition(controller, schema, credentialDefinition);
-                    break;
-                case 3: // Envia uma credencial pelo método V 1.0
-                    ConnectionRecord connectionRecord = controller.getConnections().get(0);
-                    issueCredentialV1(controller, credential, connectionRecord);
-                    break;
-                case 4: //Lista os id dos schemas criados
-                    listSchemas(controller);
-                    break;
-                case 5: //Lista os schemas através de ID
-                    listSchemaById(controller, schema.getId());
-                    break;
-                case 6: //Lista as conexões realizadas
-                    listConnections(controller);
-                    break;
-                case 7:
+        //     switch (scan.nextInt()) {
+        //         case 1: //Cria um convite de conexão
+        //             createInvitation(controller, ("Convite_" + idConvite++));
+        //             break;
+        //         case 2: //Cria uma definição de credencial
+        //             createCredentialDefinition(controller, schema, credentialDefinition);
+        //             break;
+        //         case 3: // Envia uma credencial pelo método V 1.0
+        //             ConnectionRecord connectionRecord = controller.getConnections().get(0);
+        //             issueCredentialV1(controller, credential, connectionRecord);
+        //             break;
+        //         case 4: //Lista os id dos schemas criados
+        //             listSchemas(controller);
+        //             break;
+        //         case 5: //Lista os schemas através de ID
+        //             listSchemaById(controller, schema.getId());
+        //             break;
+        //         case 6: //Lista as conexões realizadas
+        //             listConnections(controller);
+        //             break;
+        //         case 7:
 
-                    break;
-                case 8:
+        //             break;
+        //         case 8:
 
-                    break;
-                case 9:
+        //             break;
+        //         case 9:
 
-                    break;
-                case 10:
+        //             break;
+        //         case 10:
 
-                    break;
-                case 11:
+        //             break;
+        //         case 11:
 
-                    break;
-                case 12:
+        //             break;
+        //         case 12:
 
-                    break;
-                case 0:
-                    menuControl = false;
-                    break;
-                default:
-                    break;
+        //             break;
+        //         case 0:
+        //             menuControl = false;
+        //             break;
+        //         default:
+        //             break;
+        //     }
+        // } while (menuControl);
+    }
+
+    /**
+     * Realiza leitura das propriedades passadas por parâmetro ou resgata 
+     * valores presentes no arquivo de propriedade. 
+     * 
+     * @param args String[] - Dados passados na execução do projeto.
+     */
+    public static void readProperties(String[] args) {
+        try (InputStream input = AriesAgent.class.getResourceAsStream("controller.properties")) {
+            if (input == null) {
+                printlnDebug("Sorry, unable to find controller.properties.");
+                return;
             }
-        } while (menuControl);
+            Properties props = new Properties();
+            props.load(input);
+
+            endpoint = CLI.getEndpoint(args)
+                    .orElse(props.getProperty("endpoint"));
+
+            credentialDefinitionId = CLI.getCredentialDefinitionId(args)
+                    .orElse(props.getProperty("credentialDefinitionId"));
+
+            brokerIp = CLI.getBrokerIp(args)
+                    .orElse(props.getProperty("brokerIp"));
+
+            brokerPort = CLI.getBrokerPort(args)
+                    .orElse(props.getProperty("brokerPort"));
+
+            brokerPassword = CLI.getBrokerPassword(args)
+                    .orElse(props.getProperty("brokerPassword"));
+
+            brokerUsername = CLI.getBrokerUsername(args)
+                    .orElse(props.getProperty("brokerUsername"));
+
+            agentIp = CLI.getAgentIp(args)
+                    .orElse(props.getProperty("agentIp"));
+
+            agentPort = CLI.getAgentPort(args)
+                    .orElse(props.getProperty("agentPort"));
+
+        } catch (IOException ex) {
+            printlnDebug("Sorry, unable to find sensors.json or not create pesistence file.");
+        }
     }
 
     public static String createInvitation(String nodeUri) throws IOException, WriterException {
@@ -240,6 +334,16 @@ public class Main {
         }
 
         System.out.println("\nFim da lista de conexões!\n");
+    }
+
+    private static void printlnDebug(String str) {
+        if (isDebugModeValue()) {
+            System.out.println(str);
+        }
+    }
+
+    public static boolean isDebugModeValue() {
+        return DEBUG_MODE;
     }
 
 }
